97371fff3a976fa1d3db3b0ee42ff50f7c42fe06
LaiaNO <laian574@gmail.com>
2020-12-06 13:48:24 +0100

    master

--- Import_all.py
+++ Import_all.py
@@ -24,11 +24,10 @@ from Definicions import mediumchanels
 from Definicions import opteciogrups
 from Definicions import find_nearest
 from Definicions import grabt
-from Definicions import bandpower
-from Definicions import epoch_return
-from Definicions import calcul
 
 def Import_Patients(basePATH):
+
+    ### DECLARE THE 12 CHANELS GROUPS WE WANT TO REDUCE THE 62 CHANELS
     group1= ["FP2", "AFZ", "FZ", "FCZ", "CZ"]
     group2= ["F7", "F5", "F3", "F1", "AF7", "AF3", "FP1"]
     group3= ["FP2", "AF4", "AF8", "F2", "F4", "F6", "F8"]
@@ -42,20 +41,28 @@ def Import_Patients(basePATH):
     group11= ["CPZ", "PZ", "POZ", "OZ", "IZ"]
     group12= ["FC2", "FC4", "FC6", "C2", "C4", "C6"]
 
-    #X
+
+
+
+    ###SAVE THIS 12 IN A MATRIX TO BE ITERATED LATER
     groups = [group1, group2, group3, group4, group5, group6, group7, group8, group9, group10, group11, group12]
 
-    #General aspects
+    ###DECLARE WHAT TIPE ARE THIS CHANELS TO GO FROM NUMPY TO MNE
     titlegrup = ["ch1","ch2","ch3","ch4","ch5","ch6","ch7","ch8","ch9","ch10","ch11","ch12"]
     ct_ty = ["eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg", "eeg"]
     sfreq = 250
     info = mne.create_info(ch_names=titlegrup, sfreq=sfreq, ch_types=ct_ty)
 
 
-    #basePATH = 'Files/Preprocessed/'
+
+
+
+    ###READ FILES IN THE FOLDER AND SAVE THEM
+
+    #Save the list of names separated EO vs EC
     listPATHEC = []
     listPATHEO = []
-
+    #Save only the set files given that in the upload they automaticly search for the .fdt
     for root, dirs, files in os.walk(str(basePATH)):
         for filename in files:
             if '.set' in filename:
@@ -63,14 +70,20 @@ def Import_Patients(basePATH):
                     listPATHEC.append(filename)
                 if 'EO' in filename:
                     listPATHEO.append(filename)
-
+    #Save again but sorted by the name that way we know they are in order
     sorted_list_EO = sorted(listPATHEO)
     sorted_list_EC = sorted(listPATHEC)
 
+
+
+
+
+    #ONCE WE HAVE THE NAMES IS TIME TO UPLOAD THE DATA
+    #Variable were we will have all the patients with their EO and EC respective.
     EO_EC_Pacients = []
 
     for i in sorted_list_EO:
-        #LOAD
+        #Load the doc
         x=mne.io.read_raw_eeglab(basePATH+i, preload=True, verbose=True)
         #GET DATA
         data = x._data
@@ -82,7 +95,7 @@ def Import_Patients(basePATH):
         #Select the same but with EC
         EC_name = [sorted_list_EC.index(e) for e in sorted_list_EC if i[:-6] in e]
         EC_name_PATH = sorted_list_EC[EC_name[0]]
-        #LOAD
+        #LOAD EC
         x2=mne.io.read_raw_eeglab(basePATH+EC_name_PATH, preload=True, verbose=True)
         #GET DATA
         data2 = x2._data
