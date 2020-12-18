9557f9ed7399deebfb9a511aad41ffe6759b0a24
afresquet <alvarofresquet@gmail.com>
2020-12-18 13:59:45 +0100

    2nps

--- Definicions.py
+++ Definicions.py
@@ -21,51 +21,6 @@ import matplotlib.pyplot as plt
 
 #DEF
 
-'''FOR THE INITIAL DATA TREATMENT'''
-#DEF UPPER CHANNEL
-def upperchanel(chanlesnames):
-    upperchanels=[]
-    for i in chanlesnames:
-        r = i.upper()
-        upperchanels.append(r)
-    return upperchanels
-
-#DEF GRUP INFO DE CADA COMPONENT
-def group_inf(nomgroup, data_chan, upperchanels):
-    group_date = []
-
-    for e in nomgroup:
-        if e in upperchanels:
-            num = upperchanels.index(e)
-            #print(num)
-            group_date.append(data_chan[num])  
-    #print(group_date)
-    return group_date
-
-#DEF MEDIUM DELS GRUPS
-#group_dat = [[1,2,3,..., 119345],[1,2,3,..., 119345],[1,2,3,..., 119345],[1,2,3,..., 119345]]
-def mediumchanels(group_dat):
-    segundo = len(group_dat[0])
-    primero = len(group_dat)
-    mean_data = []
-    for i in range (0,segundo):
-        suma1=[]
-        for e in range(0,primero):
-            suma1.append(group_dat[e][i])
-        numpero = statistics.median(suma1)
-        mean_data.append(numpero)
-    return mean_data
-def opteciogrups(chanlesnames, groupp, data_chan):
-    group_date_finale = [] #Save all informatio chanels 12.
-    group_date_final = [] #final copy
-    upchan = upperchanel(chanlesnames)
-    for i in groupp:
-        group_date = group_inf(i, data_chan, upchan)
-        mean_group = mediumchanels(group_date)
-        mean_array = np.array(mean_group)
-        group_date_finale.append(mean_array)
-    group_date_final = np.array(group_date_finale)
-    return group_date_final
 
 
 
